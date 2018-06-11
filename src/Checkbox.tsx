2651856fe1303ffbbee048c727195caba05fdc4a
Youness MABROUK <youness.mabrouk@gmail.com>
2018-06-11 14:22:56 +0000

    added disabled prop

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -6,11 +6,12 @@ export default class Checkbox extends React.Component<CheckboxProps, any> {
   static defaultProps: Partial<CheckboxProps> = {
     indeterminate: false,
     className: '',
+    disabled: false,
     onChange: () => {}     // tslint:disable-line no-empty
   };
 
   render() {
-    const { className, checked, indeterminate, onChange } = this.props;
+    const { className, checked, indeterminate, onChange, disabled } = this.props;
     return (
       <input
         type="checkbox"
@@ -20,6 +21,7 @@ export default class Checkbox extends React.Component<CheckboxProps, any> {
           if (input) {
             input.checked = checked;
             input.indeterminate = indeterminate as boolean;
+            input.disabled = disabled as boolean;
           }
         }}
         onChange={onChange}
