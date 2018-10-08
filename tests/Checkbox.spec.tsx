3250ae8bd8638a3d8bb859f35e80e5173f81c24b
Jonathan Chiam <jczjchiam@gmail.com>
2018-10-08 17:20:24 +0800

    Remove default for onChange handler prop (#15)

--- tests/Checkbox.spec.tsx
+++ tests/Checkbox.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { configure, shallow, mount } from 'enzyme';
+import { configure, mount } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 
@@ -8,6 +8,10 @@ configure({ adapter: new Adapter() });
 import Checkbox from '../src/Checkbox';
 
 describe('<Checkbox />', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('renders checkbox', () => {
     const tree = mount(<Checkbox checked />);
     expect(toJson(tree)).toMatchSnapshot();
@@ -19,7 +23,30 @@ describe('<Checkbox />', () => {
   });
 
   it('renders checkbox with all props defined', () => {
-    const tree = mount(<Checkbox checked indeterminate className="test-class" style={{ margin: 0 }} />);
+    const tree = mount(
+      <Checkbox
+        checked
+        indeterminate
+        className="test-class"
+        style={{ margin: 0 }}
+        onChange={jest.fn()}
+      />
+    );
     expect(toJson(tree)).toMatchSnapshot();
+  });
+
+  it('onChange event triggers calls onChange prop', () => {
+    const onChange = jest.fn();
+    const wrapper = mount(<Checkbox checked onChange={onChange} />);
+
+    // find checkbox
+    const result = wrapper.find('input');
+    expect(result).toHaveLength(1);
+    const checkbox = result.at(0);
+
+    // simulate checkbox change
+    expect(onChange).not.toHaveBeenCalled();
+    checkbox.simulate('change');
+    expect(onChange).toHaveBeenCalled();
   });
 });
