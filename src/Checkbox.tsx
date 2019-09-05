6429fd3e6e046f73b1ad64267b742d59836d5208
Jonathan Chiam <jczjchiam@gmail.com>
2019-09-05 17:12:22 +0800

    Use functional component instead of class component

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -1,44 +1,36 @@
 import * as React from 'react';
 
-export interface CheckboxProps extends React.Props<Checkbox> {
+export interface CheckboxProps {
   checked: boolean;
   indeterminate?: boolean;
   className?: string;
   style?: React.CSSProperties;
   disabled?: boolean;
-  onChange?: (e: React.FormEvent) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default class Checkbox extends React.Component<CheckboxProps, any> {
-  static defaultProps: Partial<CheckboxProps> = {
-    indeterminate: false,
-    className: '',
-    style: {},
-    disabled: false
-  };
+const Checkbox = ({ checked, indeterminate = false, className = '', style = {}, disabled = false, onChange }: CheckboxProps) => {
+  return (
+    <input
+      type="checkbox"
+      className={className}
+      style={style}
+      ref={input => {
+        /* istanbul ignore else */
+        if (input) {
+          input.checked = checked;
+          input.indeterminate = indeterminate as boolean;
+          input.disabled = disabled as boolean;
+        }
+      }}
+      onChange={e => {
+        /* istanbul ignore else */
+        if (onChange) {
+          onChange(e);
+        }
+      }}
+    />
+  );
+};
 
-  render() {
-    const { className, style, checked, indeterminate, onChange, disabled } = this.props;
-    return (
-      <input
-        type="checkbox"
-        className={className}
-        style={style}
-        ref={input => {
-          /* istanbul ignore else */
-          if (input) {
-            input.checked = checked;
-            input.indeterminate = indeterminate as boolean;
-            input.disabled = disabled as boolean;
-          }
-        }}
-        onChange={(e: React.FormEvent) => {
-          /* istanbul ignore else */
-          if (onChange) {
-            onChange(e);
-          }
-        }}
-      />
-    );
-  }
-}
+export default Checkbox;
