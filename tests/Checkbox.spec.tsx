446ca289659eeecedb3d9777188aae61d5731bcf
Jonathan Chiam <jczjchiam@gmail.com>
2022-08-07 11:17:52 +0800

    Upgrade dependencies and update peer dependencies

--- tests/Checkbox.spec.tsx
+++ tests/Checkbox.spec.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
